b695f94a9cb38c34c2ff680daa7b19a6f93af900
sejun-ahn <forstudytab7604@gmail.com>
2024-11-14 12:04:59 +0900

    Initial Commit

---
+++ avtcplogger/avtcploggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  avtcploggerApp.swift
+//  avtcplogger
+//
+//  Created by mpilmini on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct avtcploggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
